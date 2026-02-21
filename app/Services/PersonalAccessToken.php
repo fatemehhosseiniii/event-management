3c911837de77f13d57169051399a92a9edc2ffd3
fatemehhosseini <fatemeh.fhosseiniii@gmail.com>
2026-02-21 19:29:17 +0330

    feat(sanctum):install sanctum for authenticate api

---
+++ app/Services/PersonalAccessToken.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
+
+class PersonalAccessToken extends SanctumPersonalAccessToken
+{
+    // ...
+}
